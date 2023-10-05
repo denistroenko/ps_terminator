d4df4875b3bf6e66df7c5c7ab879443fbb2ad67c
Denis Troenko <wcproof-grief@yandex.ru>
2023-10-05 11:10:58 +0300

    fix parser

--- src/args_parser.py
+++ src/args_parser.py
@@ -19,39 +19,42 @@ def configure_parser() -> None:
     parser.description = 'ps_terminator - console tool for ' \
                          'terminate OS processes'
 
-    # Optional argument --username
-    parser.add_argument('-u',
-                        dest='user_names',
-                        type=str,
-                        default='',
-                        action='store',
-                        help='user names for filtering processes (separator - ",")',
-                        )
+    parser.add_argument(
+            '-u',
+            dest='user_names',
+            type=str,
+            default='',
+            action='store',
+            help='user names for filtering processes (separator - ",")',
+    )
 
-    parser.add_argument('-p',
-                        dest='ps_names',
-                        type=str,
-                        default='',
-                        action='store',
-                        help='process names for filtering processes (separator - ",")',
-                        )
+    parser.add_argument(
+            '-p',
+            dest='ps_names',
+            type=str,
+            default='',
+            action='store',
+            help='process names for filtering processes (separator - ",")',
+    )
 
-    parser.add_argument('-e',
-                        dest='exclude_user_names',
-                        type=str,
-                        default='',
-                        action='store',
-                        help='exclude user names for filtering processes (separator - ",")',
-                        )
+    parser.add_argument(
+            '-e',
+            dest='exclude_user_names',
+            type=str,
+            default='',
+            action='store',
+            help='exclude user names for filtering processes (separator - ",")',
+    )
 
-    parser.add_argument('-m',
-                        '--mode',
-                        dest='mode',
-                        type=str,
-                        default='show',
-                        action='store',
-                        help='work mode: show|kill processes',
-                        )
+    parser.add_argument(
+            '-m',
+            '--mode',
+            dest='mode',
+            type=str,
+            default='show',
+            action='store',
+            help='work mode: show|kill processes',
+    )
 
 def print_parsed_args():
     print(parser.parse_args())
